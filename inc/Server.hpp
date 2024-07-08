a58acfa9471997a2f51cfc5a243ba19e4273b0fe
Bruno Manuel De Sousa Camarinha <bde-sous@c1r1s2.42porto.com>
2024-07-08 20:20:57 +0100

    trying to parse CAP and add nick to client

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -22,13 +22,14 @@ class Server
 		std::string				getPAssword() const;
 		sockaddr_in				getServerAddr() const;
 		int						getServerSocket() const;
+		std::string				getNickByFd(int fd) const;
+		void					setNickByFd(int fd, std::string nickname);				
 		void					setPort(int port);
 		void					setPassword(std::string password);
 		void					setServerAddr(sockaddr_in addr);
 		void					setServerSocket(int skt);
 		void					addClient(Client &user);
 		void					rmClient(int clientSocket, int i);
-		
 		//atributos
 		std::vector<pollfd> 	pollfds;
    		std::map<int, Client> 	clients;
