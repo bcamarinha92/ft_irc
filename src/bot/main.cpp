1c983d9ad4914736e88cc53f6076f4bc346ba569
Carolina Silva Figueiredo <csilva-f@c2r1s6.42porto.com>
2024-08-31 16:01:41 +0100

    unused files deleted

--- src/bot/main.cpp
+++ src/bot/main.cpp
@@ -1,59 +1,46 @@
-#include <iostream>
-#include <string>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+# include "../../inc/BotClient.hpp"
 
-#define IRC_SERVER "127.0.0.1"
-#define IRC_PORT 6697
 #define IRC_CHANNEL "#test"
-#define IRC_NICK "mybot"
-#define PASS "123"
 
-int main() {
-    int sock;
-    struct sockaddr_in server;
-    char buffer[256];
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+		return 1;
 
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        std::cerr << "Error creating socket" << std::endl;
-        return 1;
-    }
+	try
+	{
+		char	buffer[512];
 
-    // Set up server address
-    server.sin_family = AF_INET;
-    server.sin_port = htons(IRC_PORT);
-    inet_pton(AF_INET, IRC_SERVER, &server.sin_addr);
-    std::cout << "Connecting to " << IRC_SERVER << " on port " << IRC_PORT << std::endl;
-    // Connect to server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        std::cerr << "Error connecting to server" << std::endl;
-        return 1;
-    }
 
-    std::string passCmd = "PASS " + std::string(PASS) + "\r\n";
-    send(sock, passCmd.c_str(), passCmd.length(), 0);
+		BotClient	bot(std::string(argv[1]), atoi(argv[2]), std::string(argv[3]));
+
+		std::cout << "oi\n";
+		std::string passCmd = "PASS " + bot.getPassword() + "\r\n";
+    	send(bot.getSocket(), passCmd.c_str(), passCmd.length(), 0);
     // Send nick and user commands
-    std::string nickCmd = "NICK " + std::string(IRC_NICK) + "\r\n";
-    send(sock, nickCmd.c_str(), nickCmd.length(), 0);
-    std::string userCmd = "USER " + std::string(IRC_NICK) + " " + std::string(IRC_NICK) + " bla :mybot\r\n";
-    send(sock, userCmd.c_str(), userCmd.length(), 0);
+    	std::string nickCmd = "NICK " + bot.getNickname() + "\r\n";
+		send(bot.getSocket(), nickCmd.c_str(), nickCmd.length(), 0);
+    	std::string userCmd = "USER " + bot.getUsername() + " " + bot.getNickname() + " bla :" + bot.getRealname() + "\r\n";
+		send(bot.getSocket(), userCmd.c_str(), userCmd.length(), 0);
 
-    // Join channel
-    std::string joinCmd = "JOIN " + std::string(IRC_CHANNEL) + "\r\n";
-    send(sock, joinCmd.c_str(), joinCmd.length(), 0);
+		// Join channel
+    	std::string joinCmd = "JOIN " + std::string(IRC_CHANNEL) + "\r\n";
+    	send(bot.getSocket(), joinCmd.c_str(), joinCmd.length(), 0);
 
-    // Send message
-    std::string msgCmd = "PRIVMSG " + std::string(IRC_CHANNEL) + " :Hello, world!\r\n";
-    send(sock, msgCmd.c_str(), msgCmd.length(), 0);
+    	// Send message
+    	std::string msgCmd = "PRIVMSG " + std::string(IRC_CHANNEL) + " :Hello, world!\r\n";
+    	send(bot.getSocket(), msgCmd.c_str(), msgCmd.length(), 0);
 
-    // Receive and print messages
-    while (true) {
-        recv(sock, buffer, 256, 0);
-        std::cout << buffer << std::endl;
-    }
-
+		// Receive and print messages
+    	while (true)
+		{
+        	recv(bot.getSocket(), buffer, 256, 0);
+        	std::cout << buffer << std::endl;
+    	}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
     return 0;
 }
